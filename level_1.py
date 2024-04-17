c7a3458378049edf2ed7aab931c0f60ec4854c8a
tonywdm7 <buten.anton@gmail.com>
2024-04-17 17:43:07 -0400

    first level work good

--- level_1.py
+++ level_1.py
@@ -38,12 +38,17 @@ def start_level1():
 
         def collision_check(self, player_rect):
             return self.hitbox.colliderect(player_rect)
+
+    portal_img = pygame.image.load("portal.png")
+    heart_img = pygame.image.load("heart.png")
+
     class Portal(pygame.sprite.Sprite):
         def __init__(self, x, y):
             super().__init__()
             self.image = portal_img
-            self.rect = self.image.get_rect
-            self.rect = (x, y)
+            self.rect = self.image.get_rect()
+            self.rect.x = x
+            self.rect.y = y
 
         def draw(self, screen):
             screen.blit(self.image, self.rect)
@@ -52,11 +57,19 @@ def start_level1():
         def __init__(self, x, y):
             super().__init__()
             self.image = heart_img
-            self.rect = self.image.get_rect
-            self.rect = (x, y)
+            self.rect = self.image.get_rect()
+            self.rect.x = x
+            self.rect.y = y
+
 
         def draw(self, screen):
             screen.blit(self.image, self.rect)
+
+    hearts = [Heart(10, 10), Heart(40, 10), Heart(70, 10)]
+    portal = Portal(380, 50)
+
+    health = 3
+    spawn_position = (100, 50)
 
     platforms = []
     def add_platform(x, y, width, height):
@@ -96,7 +109,7 @@ def start_level1():
         if keys[pygame.K_d]:
             player_rect.x += player_speed
         if keys[pygame.K_a]:
-            player_rect.x += player_speed
+            player_rect.x -= player_speed
 
         if not jumping:
             if keys[pygame.K_SPACE] and collide_check:
@@ -109,16 +122,32 @@ def start_level1():
                 jumping = False
                 jump_speed = 15
 
+        if player_rect.y > SCREEN_HEIGHT:
+            hearts.pop(-1)
+            health -= 1
+            player_rect.topleft = spawn_position
+
+        if health == 0:
+            running = False
+
+        if portal.rect.colliderect(player_rect):
+            running = False
+
         if not collide_check and jump_speed >= 0:
             player_rect.y += gravity
-
-
 
         screen.blit(background, (0, 0))
         screen.blit(player_image, player_rect)
         for plt in platforms:
             plt.draw(screen)
 
+        for h in hearts:
+            h.draw(screen)
+
+        portal.draw(screen)
+
+
+        collide_check = False
         pygame.display.update()
         pygame.time.Clock().tick(60)
 
